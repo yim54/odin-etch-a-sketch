0456de06389e5ea5506a5af45d775d61cdcda18b
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-06 18:12:58 -0700

    Add draw when hover over square

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const grid = document.querySelector("#grid");
 
+// create grid
+
 for (let i = 0; i < 16 * 16; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
     grid.appendChild(square);
+}
+
+const squares = document.querySelectorAll(".square");
+for (const square of squares) {
+    square.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "gray";
+    })
 }
