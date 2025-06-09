6eda265a13bcabfe2cb21347d7e2719166bccdb5
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-09 14:19:04 -0700

    Add resize functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector("#grid");
+let  grid = document.querySelector("#grid");
 
 // create grid
 
@@ -15,7 +15,7 @@ grid.addEventListener("mousedown", () => {
     mousedown = true;
 })
 
-grid.addEventListener("mouseover", (event) => {
+function draw(event) {
     // check if target is the container itself
 
     if (event.target.id == "grid") {
@@ -27,15 +27,48 @@ grid.addEventListener("mouseover", (event) => {
     if (mousedown) {
         event.target.style.backgroundColor = "gray";
     }
-});
+}
+
+grid.addEventListener("mouseover", draw);
 
 // check mouse release
 
 document.addEventListener("mouseup", () => {
     mousedown = false;
-})
+});
 
 document.addEventListener("dragstart", (event)=> {
     event.preventDefault();
-})
+});
 
+let button = document.querySelector("#button");
+button.addEventListener("click", (event) => {
+    // get new size
+
+    let dim = prompt("How many squares per side? (max 100)");
+    dim = Math.min(dim, 100);
+    dim = Math.max(dim, 1);
+    console.log(dim);
+
+    // create new grid
+
+    grid.remove();
+    grid = document.createElement("div");
+    grid.id = "grid";
+    document.body.appendChild(grid);
+    for (let i = 0; i < dim * dim; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flexBasis = `${1/dim * 100}%`;
+        grid.appendChild(square);
+    }
+
+    // add drawing functionality
+
+    grid.addEventListener("mousedown", () => {
+        mousedown = true;
+    })
+    grid.addEventListener("mouseover", draw);
+
+
+});
