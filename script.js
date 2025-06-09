002c15da814f165a37cebbcd35cbcffe22633b2b
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-09 13:21:31 -0700

    Add comments

--- script.js
+++ script.js
@@ -8,20 +8,30 @@ for (let i = 0; i < 16 * 16; i++) {
     grid.appendChild(square);
 }
 
+let mousedown = false;
+
+grid.addEventListener("mousedown", () => {
+    mousedown = true;
+})
+
 grid.addEventListener("mouseover", (event) => {
-    console.log(event.target)
-    if (event.buttons == 1) {
+    // check if target is the container itself
+
+    if (event.target.id == "grid") {
+        return;
+    }
+
+    // check if mouse button is down
+
+    if (mousedown) {
         event.target.style.backgroundColor = "gray";
     }
 });
 
-/*
-const squares = document.querySelectorAll(".square");
-for (const square of squares) {
-    square.addEventListener("mouseenter", (event) => {
-        if (event.buttons == 1) {
-            event.target.style.backgroundColor = "gray";
-        }
-    })
-}
-    */
+document.addEventListener("mouseup", () => {
+    mousedown = false;
+})
+
+document.addEventListener("dragstart", (event)=> {
+    event.preventDefault();
+})
